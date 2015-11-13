84140bac10346ca832cc87a30d57cf738dbe17a8
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-11-13 16:19:38 -0700

    Using requirements.txt and setup.py to fix Travis CI

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages, Command
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as output:
+        return output.read()
+
+requirements = ['unidecode', 'wikipedia', 'whoosh', 'nltk', 'scikit-learn', 'regex', 'fuzzywuzzy',
+                'six']
+
+
+class DownloadCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import nltk
+        nltk.download('stopwords')
+        nltk.download('punkt')
+
+setup(
+    name='qb',
+    version='0.0.1',
+    description='Quiz Bowl AI system named QANTA',
+    license='MIT',
+    long_description=read('README.md'),
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    cmdclass={'download': DownloadCommand}
+)
