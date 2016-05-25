0305f86c3eae22114b936569446c02de68a63c3f
Jordan Boyd-Graber <jordanbg@gmail.com>
2016-05-25 09:39:16 -0600

    feature combinations

---
+++ feature_config.py
@@ -0,0 +1,12 @@
+from collections import defaultdict, OrderedDict
+
+kFEATURES = OrderedDict([
+    # ("ir", None),
+    ("lm", None),
+    ("deep", None),
+    # ("answer_present", None),
+    # ("text", None),
+    # ("classifier", None),
+    # ("wikilinks", None),
+    # ("mentions", None),
+    ])
