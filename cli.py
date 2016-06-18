cc05a825a92524ac245b14102b867f351e29bc9b
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-06-18 15:29:32 -0600

    Start switching to using logging instead of print statements

--- cli.py
+++ cli.py
@@ -1,15 +1,22 @@
+import logging
+
 import click
 
 from qanta.extractors import mentions
+from qanta.util.environment import ENVIRONMENT
 from qanta.streaming import start_qanta_streaming, start_spark_streaming
 
+
+log = logging.getLogger(__name__)
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 def main():
-    pass
+    log.info("QANTA starting with configuration:")
+    for k, v in ENVIRONMENT:
+        log.info("{0}={1}".format(k, v))
 
 
 @main.command()
@@ -20,11 +27,6 @@ def spark_stream():
 @main.command()
 def qanta_stream():
     start_qanta_streaming()
-
-
-@main.command()
-def train_clm():
-    pass
 
 
 @main.command()
