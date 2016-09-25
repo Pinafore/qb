1a8a89241b305873ab0be4f31d48d8f9e47662bb
Mohamad Khouja <mkhooja@gmail.com>
2016-09-25 20:34:13 +0000

    Cleaning

--- qanta/guesser/classify/learn_classifiers.py
+++ qanta/guesser/classify/learn_classifiers.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 
-#from functional import seq
+from functional import seq
 
 from qanta import logging
 from qanta.guesser.util.functions import relu
@@ -58,10 +58,10 @@ def compute_recall_accuracy():
 
             if sent_position + 1 == len(qs):
                 p_dist = classifier.predict_proba(curr_feats)
-                #correct = seq(zip(p_dist[0], class_labels))\
-                #    .sorted(reverse=True)\
-                #    .take(N_GUESSES)\
-                #    .exists(lambda s: ans == s[1])
+                correct = seq(zip(p_dist[0], class_labels))\
+                    .sorted(reverse=True)\
+                    .take(N_GUESSES)\
+                    .exists(lambda s: ans == s[1])
                 accuracy += int(class_labels[p_dist.argmax()] == ans)
                 recall += correct
                 if not correct:
