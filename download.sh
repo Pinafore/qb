08991d5008a74a3b420d21db93c4c8577a58e5c4
Fenfei Guo <guofenfei1991@gmail.com>
2016-11-27 14:49:32 -0700

    add download file

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 
 aws s3 cp s3://entilzha-us-west-2/wiki-network/titles-sorted.txt /ssd-c/qanta
 aws s3 cp s3://entilzha-us-west-2/wiki-network/links-simple-sorted.txt /ssd-c/qanta
-aws s3 cp wiki-network_distance.ipynb s3://entilzha-us-west-2/wiki-network/notebooks/fenfei/wiki-network.ipynb
+aws s3 cp ./notebooks/wiki-network-fenfei.ipynb s3://entilzha-us-west-2/wiki-network/notebooks/fenfei/wiki-network.ipynb
 
