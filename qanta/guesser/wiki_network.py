4b3ddff5594710f2803cf333659ed18d925fd514
Fenfei Guo <guofenfei1991@gmail.com>
2016-12-05 02:07:11 -0700

    guesser based on sense embeddings

--- qanta/guesser/wiki_network.py
+++ qanta/guesser/wiki_network.py
@@ -34,10 +34,29 @@ class WikiNetworkGuesser:
         self.g = g
         self.vertex_set = vertex_set
         self.page_map = page_map
+        params = pickle.load(open('/ssd-c/qanta/best_embs.pkl','rb'), encoding='latin1')
+        self.word_emb_multi=params['word_emb_multi']
+        self.word_emb_single=params['word_emb_single']
+        self.context_emb = params['context_emb']
+        vocabs = pickle.load(open('/ssd-c/qanta/wiki_vocabs_p3.pkl', 'rb'))
+        self.word_list = vocabs['word_list']
+        self.word_index = vocabs['word_index']
+        self.reverse_map = vocabs['reverse_map']
+        self.multi_words = vocabs['multi_words']
+        self.multi_idx_map = vocabs['multi_idx_map']
+        self.single_idx_map = vocabs['single_idx_map']
 
     def tokenize(self, text):
         raw_words = text.lower().replace(',', '').replace('.', '').split()
         return [w for w in raw_words if w not in self.stop_words]
+    
+    def emb_tokenize(self, text):
+        f = string.punctuation
+        f = f.replace("'", '')
+        f += '\t\n'
+        text = text.translate(str.maketrans(f, ' ' * len(f)))
+        raw_words = text.lower().replace(',', '').replace('.', '').split(' ')
+        return [w for w in raw_words if w not in self.stop_words and w is not '']
 
     def generate_guesses(self, text, answer, qnum):
         words = self.tokenize(text)
@@ -67,6 +86,47 @@ class WikiNetworkGuesser:
             columns['qnum'].append(qnum)
 
         return pd.DataFrame(columns), seed_vertexes
+    
+    def generate_guesses_emb(self, text, answer, qnum):
+        words = self.tokenize(text)
+        seed_vertexes = {self.page_map[w] for w in words if w in self.page_map}
+        candidate_vertexes = set()
+        cw_id = [self.word_index[w] for w in self.emb_tokenize(text) if self.word_index.get(w)]
+        c_emb = np.mean(self.context_emb[cw_id], axis=0)
+        
+        for seed_v in seed_vertexes:
+            for v in self.g.neighbors(seed_v):
+                if v not in seed_vertexes:
+                    candidate_vertexes.add(v)
+
+        seed_distances = defaultdict(int)
+        for u in candidate_vertexes:
+            page = self.g.node[u]['page']
+            pw_ids = [self.word_index[w] for w in page.split('_') if self.word_index.get(w)]
+            v_embs = []
+            for wi in pw_ids:
+                if self.multi_words.get(wi):
+                    s_embs = self.word_emb_multi[self.reverse_map[wi]]
+                    prds = np.dot(s_embs, c_emb)
+                    v_embs.append(s_embs[np.argmax(prds)])
+                else:
+                    v_embs.append(self.word_emb_single[self.reverse_map[wi]])
+            if len(v_embs) > 0:
+                v_embs = np.mean(np.array(v_embs), axis=0)
+                seed_distances[u] = sigmoid(np.dot(v_embs, c_emb))
+            else:
+                seed_distances[u] = 0.0
+
+        columns = {'vid': [], 'distance': [], 'page': [], 'answer': [], 'qnum': []}
+
+        for vid in candidate_vertexes:
+            columns['vid'].append(vid)
+            columns['page'].append(self.g.node[vid]['page'])
+            columns['distance'].append(seed_distances[vid])
+            columns['answer'].append(answer)
+            columns['qnum'].append(qnum)
+
+        return pd.DataFrame(columns), seed_vertexes
 
 
 def evaluate():
@@ -78,7 +138,7 @@ def evaluate():
     subset = questions[0:10]
     df = None
     for q in subset:
-        tmp_df = wiki.generate_guesses(q.flatten_text(), q.page.lower().replace(' ', '_'), q.qnum)
+        tmp_df = wiki.generate_guesses_emb(q.flatten_text(), q.page.lower().replace(' ', '_'), q.qnum)
         if df is None:
             df = tmp_df
         else:
