d5ec04ddca276bc1afaa94db440724cccba7d922
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-01-16 11:23:48 -0700

    Remove old dan code

--- qanta/pipeline/guesser/dan.py
+++ qanta/pipeline/guesser/dan.py
@@ -3,9 +3,3 @@ from luigi import LocalTarget, Task, WrapperTask
 
 from qanta.util import constants as c
 from qanta.util import environment as e
-from qanta.guesser import dan
-from qanta.guesser.classify.learn_classifiers import print_recall_at_n
-
-
-
-
