3a43bb5622d0fb638dff2ced7d02d73723d704a8
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-02-03 15:09:35 -0700

    Upgrade python and tensorflow

--- packer/bin/install-python.sh
+++ packer/bin/install-python.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Install Python 3.5 and the Scipy Stack
-wget https://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
-bash Anaconda3-4.1.1-Linux-x86_64.sh -b
-rm Anaconda3-4.1.1-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+bash Anaconda3-4.3.0-Linux-x86_64.sh -b
+rm Anaconda3-4.3.0-Linux-x86_64.sh
 echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
 echo "export PYTHONPATH=$PYTHONPATH:/home/ubuntu/dependencies/spark-2.1.0-bin-hadoop2.7/python" >> ~/.bashrc
 echo "export SPARK_HOME=/home/ubuntu/dependencies/spark-2.1.0-bin-hadoop2.7" >> ~/.bashrc
