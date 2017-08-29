d15d41364df9538fbb78fc944bb860f678db5661
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-08-29 12:45:57 -0600

    Fix tf aws downloads

--- terraform/terraform-aws-downloads.sh
+++ terraform/terraform-aws-downloads.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-PS1=non-empty source ~/.bashrc
-bash terraform/aws-downloads.sh
+
+/home/ubuntu/anaconda3/bin/python setup.py download
+
+mkdir -p data/external/wikipedia
+/home/ubuntu/anaconda3/bin/aws s3 cp s3://pinafore-us-west-2/public/wiki_redirects.csv data/external/wikipedia/all_wiki_redirects.csv
+
+mkdir -p data/external/deep
+/home/ubuntu/anaconda3/bin/aws s3 cp s3://pinafore-us-west-2/public/glove.6B.300d.txt data/external/deep/glove.6B.300d.txt
