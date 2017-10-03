4c257c43f716c3993fed1f0ab0f8b97b6288a809
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-10-02 20:59:47 -0400

    Fix bad flag placement

--- packer/bin/install-dl-libs-gpu.sh
+++ packer/bin/install-dl-libs-gpu.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/home/ubuntu/anaconda3/bin/conda -y install pytorch torchvision cuda80 -c soumith
+/home/ubuntu/anaconda3/bin/conda install -y pytorch torchvision cuda80 -c soumith
 /home/ubuntu/anaconda3/bin/pip install tensorflow-gpu
 /home/ubuntu/anaconda3/bin/pip install keras
