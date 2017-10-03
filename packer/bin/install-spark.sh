a7287ae8097c6b4c3ac9e80a9420e7c3daee77f6
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-10-02 22:44:11 -0400

    spark packer directory typo

--- packer/bin/install-spark.sh
+++ packer/bin/install-spark.sh
@@ -8,5 +8,5 @@ wget https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
 mkdir -p /home/ubuntu/dependencies/
 tar xzf spark-2.2.0-bin-hadoop2.7.tgz -C /home/ubuntu/dependencies
 rm spark-2.2.0-bin-hadoop2.7.tgz
-mv /home/ubuntu/spark-defaults.conf /home/ubuntu/spark-2.2.0-bin-hadoop2.7/conf
+mv /home/ubuntu/spark-defaults.conf /home/ubuntu/dependencies/spark-2.2.0-bin-hadoop2.7/conf
 echo "export PYSPARK_PYTHON=/home/ubuntu/anaconda3/bin/python3" >> ~/.bashrc
