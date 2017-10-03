2720d553b27b122b831def47f84da2699cf9f73a
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-10-02 21:21:42 -0400

    Tweak spark install

--- packer/bin/install-spark.sh
+++ packer/bin/install-spark.sh
@@ -4,7 +4,7 @@ set -e
 
 # Install Apache Spark
 wget https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
-tar -xvzf spark-2.2.0-bin-hadoop2.7.tgz
+tar xzf spark-2.2.0-bin-hadoop2.7.tgz
 rm spark-2.2.0-bin-hadoop2.7.tgz
-mv spark-2.2.0-bin-hadoop2.7 ~/dependencies
+mv spark-2.2.0-bin-hadoop2.7 /home/ubuntu/dependencies
 echo "export PYSPARK_PYTHON=/home/ubuntu/anaconda3/bin/python3" >> ~/.bashrc
