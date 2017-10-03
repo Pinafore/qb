fb30301afd9a2da010dff52d3b066a2979331448
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-10-02 21:48:35 -0400

    Spark install issue

--- packer/bin/install-spark.sh
+++ packer/bin/install-spark.sh
@@ -3,8 +3,10 @@
 set -e
 
 # Install Apache Spark
+cd /home/ubuntu
 wget https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
 tar xzf spark-2.2.0-bin-hadoop2.7.tgz
 rm spark-2.2.0-bin-hadoop2.7.tgz
 mv spark-2.2.0-bin-hadoop2.7 /home/ubuntu/dependencies
+mv /home/ubuntu/spark-defaults.conf /home/ubuntu/spark-2.2.0-bin-hadoop2.7/conf
 echo "export PYSPARK_PYTHON=/home/ubuntu/anaconda3/bin/python3" >> ~/.bashrc
