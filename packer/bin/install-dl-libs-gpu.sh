36daf74616b3dd7b489cd8f74b290b254e3897cc
EntilZha <ski.rodriguez@gmail.com>
2017-11-01 18:49:53 -0600

    Packer scripts for cuda 9 pytorch

--- packer/bin/install-dl-libs-gpu.sh
+++ packer/bin/install-dl-libs-gpu.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-/home/ubuntu/anaconda3/bin/conda install -y pytorch torchvision cuda80 -c soumith
 /home/ubuntu/anaconda3/bin/pip install tensorflow-gpu
 /home/ubuntu/anaconda3/bin/pip install keras
+
+cd /home/ubuntu
+git clone --recursive https://github.com/pytorch/pytorch.git
+cd /home/ubuntu/pytorch
+git checkout 66d24c5
+git submodule update --recursive
