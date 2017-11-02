b00df5c1dac0d6dd1f739867740a9c3e08e701b1
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-11-01 23:19:45 -0600

    New packer GPU image built, need to do same for CPU

--- bin/init.sh
+++ bin/init.sh
@@ -5,8 +5,6 @@
 cd /home/ubuntu/dependencies/spark-2.2.0-bin-hadoop2.7
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/ubuntu/cuda:/usr/local/cuda/lib64:/usr/local/extras/CUPTI/lib64" >> conf/spark-env.sh
 echo "export CUDA_HOME=/usr/local/cuda" >> conf/spark-env.sh
-/home/ubuntu/anaconda3/bin/pip install spacy pycountry
-/home/ubuntu/anaconda3/bin/python -m spacy download en
 
 cd /ssd-c/qanta/qb
 bash packer/bin/install-elasticsearch.sh
