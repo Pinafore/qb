4b9b141c1d152c3ae96d1a4c5889858912fce020
EntilZha <ski.rodriguez@gmail.com>
2017-12-20 19:13:24 -0700

    Fix dropout typo

--- packer/bin/install-dl-libs-gpu.sh
+++ packer/bin/install-dl-libs-gpu.sh
@@ -4,5 +4,5 @@ set -e
 
 /home/ubuntu/anaconda3/bin/pip install tensorflow-gpu
 /home/ubuntu/anaconda3/bin/pip install keras
-/home/ubuntu/anaconda3/bin/pip install pip install git+https://github.com/salesforce/cove.git
 /home/ubuntu/anaconda3/bin/conda install -y pytorch torchvision cuda90 -c pytorch
+/home/ubuntu/anaconda3/bin/pip install git+https://github.com/salesforce/cove.git
