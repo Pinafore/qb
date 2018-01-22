9efa1503488250972c9a2f7f6e4e1cdce130c356
EntilZha <ski.rodriguez@gmail.com>
2018-01-22 13:58:04 -0700

    Update tied model code

--- qanta/guesser/tied.py
+++ qanta/guesser/tied.py
@@ -62,109 +62,6 @@ re_pattern = '|'.join([re.escape(p) for p in qb_patterns])
 re_pattern += r'|\[.*?\]|\(.*?\)'
 
 
-class TiedLinear(nn.Linear):
-    def __init__(self, group, w_type, in_features, out_features):
-        """
-        :param group: Group name to tie weights together
-        :param w_type: One of: "general", "source", or "target"
-        :param in_features: Passed to nn.Linear
-        :param out_features: Passed to nn.Linear
-        """
-        super().__init__(in_features, out_features)
-        self.group = group
-        self.w_type = w_type
-
-
-class TiedEmbeddings(nn.Embedding):
-    def __init__(self, group, w_type, num_embeddings, embedding_dim):
-        """
-        :param group: Group name to tie weights together
-        :param w_type: One of: "general", "source", or "target"
-        :param num_embeddings: Passed to nn.Embedding
-        :param embedding_dim: Passed to nn.Embedding
-        """
-        super().__init__(num_embeddings, embedding_dim)
-        self.group = group
-        self.w_type = w_type
-
-
-class TiedAdam(Optimizer):
-    """Implements Adam algorithm.
-
-    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
-
-    Arguments:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float, optional): learning rate (default: 1e-3)
-        betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-
-    .. _Adam\: A Method for Stochastic Optimization:
-        https://arxiv.org/abs/1412.6980
-    """
-
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0):
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay)
-        super(TiedAdam, self).__init__(params, defaults)
-
-    def step(self, closure=None):
-        """Performs a single optimization step.
-
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-
-        for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
-
-                state = self.state[p]
-
-                # State initialization
-                if len(state) == 0:
-                    state['step'] = 0
-                    # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
-
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                beta1, beta2 = group['betas']
-
-                state['step'] += 1
-
-                if group['weight_decay'] != 0:
-                    grad = grad.add(group['weight_decay'], p.data)
-
-                # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-
-                denom = exp_avg_sq.sqrt().add_(group['eps'])
-
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
-
-                p.data.addcdiv_(-step_size, exp_avg, denom)
-
-        return loss
-
-
 class DanEncoder(nn.Module):
     def __init__(self, embedding_dim, n_hidden_layers, n_hidden_units, dropout_prob):
         super(DanEncoder, self).__init__()
@@ -187,11 +84,11 @@ class DanEncoder(nn.Module):
         return self.encoder(x_array)
 
 
-class Model(nn.Module):
+class TiedModel(nn.Module):
     def __init__(self, text_field: Field, n_classes,
                  init_embeddings=True, emb_dim=300,
                  n_hidden_units=1000, n_hidden_layers=1, nn_dropout=.265, sm_dropout=.158):
-        super(Model, self).__init__()
+        super(TiedModel, self).__init__()
         vocab = text_field.vocab
         self.vocab_size = len(vocab)
         self.emb_dim = emb_dim
@@ -202,24 +99,57 @@ class Model(nn.Module):
         self.sm_dropout = sm_dropout
 
         self.dropout = nn.Dropout(nn_dropout)
-        self.embeddings = nn.Embedding(self.vocab_size, emb_dim, padding_idx=vocab.stoi[text_field.pad_token])
+        pad_idx = vocab.stoi[text_field.pad_token]
+        self.general_embeddings = nn.Embedding(self.vocab_size, emb_dim, padding_idx=pad_idx)
+        self.qb_embeddings = nn.Embedding(self.vocab_size, emb_dim, padding_idx=pad_idx)
+        self.wiki_embeddings =nn.Embedding(self.vocab_size, emb_dim, padding_idx=pad_idx)
+        qb_mask = torch.cat([torch.ones(1, 600), torch.zeros(1, 300)], dim=1)
+        wiki_mask = torch.cat([torch.ones(1, 300), torch.zeros(1, 300), torch.ones(1, 300)], dim=1)
+        self.combined_mask = torch.cat([qb_mask, wiki_mask], dim=0).float().cuda()
+
         if init_embeddings:
             mean_emb = vocab.vectors.mean(0)
             vocab.vectors[vocab.stoi[text_field.unk_token]] = mean_emb
-            self.embeddings.weight.data = vocab.vectors.cuda()
-        self.encoder = DanEncoder(emb_dim, self.n_hidden_layers, self.n_hidden_units, self.nn_dropout)
+            self.general_embeddings.weight.data = vocab.vectors.cuda()
+            self.qb_embeddings.weight.data = vocab.vectors.cuda()
+            self.wiki_embeddings.weight.data = vocab.vectors.cuda()
+
+        # One averaged embedding for each of general, qb, and wiki
+        self.encoder = DanEncoder(3 * emb_dim, self.n_hidden_layers, self.n_hidden_units, self.nn_dropout)
         self.classifier = nn.Sequential(
             nn.Linear(self.n_hidden_units, n_classes),
             nn.BatchNorm1d(n_classes),
             nn.Dropout(self.sm_dropout)
         )
 
-    def forward(self, input_: Variable, lengths):
+    def forward(self, input_: Variable, lengths, qnums):
+        """
+        :param input_: [batch_size, seq_len] of word indices
+        :param lengths: Length of each example
+        :param qnums: QB qnum if a qb question, otherwise -1 for wikipedia, used to get domain as source/target
+        :return:
+        """
         if not isinstance(lengths, Variable):
             lengths = Variable(lengths.float(), volatile=not self.training)
-        encoded = self.embeddings(input_)
-        encoded = encoded.sum(1) / lengths.float().view(input_.size()[0], -1)
-        encoded = self.encoder(self.dropout(encoded))
+
+        g_embed = self.general_embeddings(input_)
+        g_embed = g_embed.sum(1) / lengths.float().view(input_.size()[0], -1)
+        g_embed = self.dropout(g_embed)
+
+        qb_embed = self.qb_embeddings(input_)
+        qb_embed = qb_embed.sum(1) / lengths.float().view(input_.size()[0], -1)
+        qb_embed = self.dropout(qb_embed)
+
+        wiki_embed = self.wiki_embeddings(input_)
+        wiki_embed = wiki_embed.sum(1) / lengths.float().view(input_.size()[0], -1)
+        wiki_embed = self.dropout(wiki_embed)
+
+        # Need to use qnum to mask either qb or wiki embeddings here
+        concat_embed = torch.cat([g_embed, qb_embed, wiki_embed], dim=1)
+        mask = Variable(self.combined_mask[(qnums < 0).long()])
+        masked_embed = concat_embed * mask
+
+        encoded = self.encoder(masked_embed)
         return self.classifier(encoded)
 
 
@@ -239,6 +169,7 @@ class TiedGuesser(AbstractGuesser):
         self.n_wiki_sentences = guesser_conf['n_wiki_sentences']
         self.wiki_title_replace_token = guesser_conf['wiki_title_replace_token']
         self.lowercase = guesser_conf['lowercase']
+        self.tied_l2 = guesser_conf['tied_l2']
 
         self.page_field: Optional[Field] = None
         self.qnum_field: Optional[Field] = None
@@ -280,7 +211,7 @@ class TiedGuesser(AbstractGuesser):
         log.info(f'Vocab={len(self.text_field.vocab)}')
 
         log.info('Initializing Model')
-        self.model = Model(
+        self.model = TiedModel(
             self.text_field, self.n_classes, emb_dim=self.emb_dim,
             n_hidden_units=self.n_hidden_units, n_hidden_layers=self.n_hidden_layers,
             nn_dropout=self.nn_dropout, sm_dropout=self.sm_dropout,
@@ -325,14 +256,25 @@ class TiedGuesser(AbstractGuesser):
         for batch in iterator:
             text, lengths = batch.text
             page = batch.page
+            qnums = batch.qnum.cuda()
 
             if is_train:
                 self.model.zero_grad()
 
-            out = self.model(text, lengths)
+            out = self.model(text, lengths, qnums)
             _, preds = torch.max(out, 1)
             accuracy = torch.mean(torch.eq(preds, page).float()).data[0]
             batch_loss = self.criterion(out, page)
+            if self.tied_l2 != 0:
+                w_general = self.model.general_embeddings.weight
+                w_source = self.model.wiki_embeddings.weight
+                w_target = self.model.qb_embeddings.weight
+                tied_weight_l2 = self.tied_l2 / 2 * (
+                        (w_general ** 2).sum() +
+                        ((w_source - w_general) ** 2).sum() +
+                        ((w_target - w_general) ** 2).sum()
+                )
+                batch_loss += tied_weight_l2
             if is_train:
                 batch_loss.backward()
                 torch.nn.utils.clip_grad_norm(self.model.parameters(), self.gradient_clip)
@@ -348,8 +290,9 @@ class TiedGuesser(AbstractGuesser):
     def guess(self, questions: List[QuestionText], max_n_guesses: Optional[int]):
         examples = [self.text_field.preprocess(q) for q in questions]
         text, lengths = self.text_field.process(examples, None, False)
+        qnums = self.qnum_field.process([0 for _ in questions]).cuda()
         guesses = []
-        out = self.model(text, lengths)
+        out = self.model(text, lengths, qnums)
         probs = F.softmax(out)
         scores, preds = torch.max(probs, 1)
         scores = scores.data.cpu().numpy()
@@ -380,7 +323,8 @@ class TiedGuesser(AbstractGuesser):
                 'use_wiki': self.use_wiki,
                 'n_wiki_sentences': self.n_wiki_sentences,
                 'wiki_title_replace_token': self.wiki_title_replace_token,
-                'lowercase': self.lowercase
+                'lowercase': self.lowercase,
+                'tied_l2': self.tied_l2
             }, f)
 
     @classmethod
@@ -404,7 +348,8 @@ class TiedGuesser(AbstractGuesser):
         guesser.n_wiki_sentences = params['n_wiki_sentences']
         guesser.wiki_title_replace_token = params['wiki_title_replace_token']
         guesser.lowercase = params['lowercase']
-        guesser.model = Model(
+        guesser.tied_l2 = params['tied_l2']
+        guesser.model = TiedModel(
             guesser.text_field, guesser.n_classes,
             init_embeddings=False, emb_dim=guesser.emb_dim
         )
