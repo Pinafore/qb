aef002958401a9a6fff1c7d13aa40dae4a676331
EntilZha <ski.rodriguez@gmail.com>
2018-01-25 18:59:35 -0700

    Remove aws key input in favor of IAM roles

--- aws.tf
+++ aws.tf
@@ -147,6 +147,11 @@ resource "aws_security_group" "qanta_internal" {
   }
 }
 
+data "aws_iam_instance_profile" "s3_instance_profile" {
+  name = "s3-full-access"
+}
+
+
 #  _____ ____ ____    ___           _
 # | ____/ ___|___ \  |_ _|_ __  ___| |_ __ _ _ __   ___ ___  ___
 # |  _|| |     __) |  | || '_ \/ __| __/ _` | '_ \ / __/ _ \/ __|
@@ -161,6 +166,7 @@ resource "aws_spot_instance_request" "qanta" {
   spot_type = "one-time"
   wait_for_fulfillment = true
   count = "${var.instance_count}"
+  iam_instance_profile = "${data.aws_iam_instance_profile.s3_instance_profile.name}"
 
   vpc_security_group_ids = [
     "${aws_security_group.qanta_internal.id}",
@@ -194,20 +200,6 @@ resource "aws_spot_instance_request" "qanta" {
 
   provisioner "remote-exec" {
     script = "terraform/configure-swap.sh"
-  }
-
-  # Configure AWS credentials
-  provisioner "remote-exec" {
-    inline = [
-      "echo \"export AWS_ACCESS_KEY_ID=${var.access_key}\" >> /home/ubuntu/dependencies/spark-2.2.0-bin-hadoop2.7/conf/spark-env.sh",
-      "echo \"export AWS_ACCESS_KEY_ID=${var.access_key}\" >> /home/ubuntu/.bashrc",
-      "echo \"export AWS_SECRET_ACCESS_KEY=${var.secret_key}\" >> /home/ubuntu/dependencies/spark-2.2.0-bin-hadoop2.7/conf/spark-env.sh",
-      "echo \"export AWS_SECRET_ACCESS_KEY=${var.secret_key}\" >> /home/ubuntu/.bashrc",
-      "mkdir -p /home/ubuntu/.aws",
-      "echo \"[default]\" >> /home/ubuntu/.aws/credentials",
-      "echo \"aws_access_key_id = ${var.access_key}\" >> /home/ubuntu/.aws/credentials",
-      "echo \"aws_secret_access_key = ${var.secret_key}\" >> /home/ubuntu/.aws/credentials",
-    ]
   }
 
   # Configure qanta environment variables
