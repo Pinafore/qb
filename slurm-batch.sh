42801c139d0767a65c7aae142b77dda22c2e786f
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 16:51:16 -0700

    Add logging

--- slurm-batch.sh
+++ slurm-batch.sh
@@ -2,9 +2,11 @@
 
 #SBATCH --job-name=qanta
 #SBATCH --qos=gpu
-#SBATCH --nodes=5
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
+#SBATCH --output qanta.out.%j
+#SBATCH --error qanta.out.%j
 
 srun -N 1 -c "slurm-run.sh 0" &
 srun -N 1 -c "slurm-run.sh 1" &
