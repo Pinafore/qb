a1b6145b66ec2eb3eae185f70bdb1b5e8f16b995
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 16:41:29 -0700

    First attempt at slurm scripts

---
+++ slurm-run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p /scratch0/qanta/"$1"/
+cp -r /fs/clip-quiz/qb /scratch0/qanta/"$1"/
+rm -rf /scratch0/qanta/"$1"/qb/output/guesser
+ls /scratch0/qanta/"$1"/qb
+nvidia-smi
