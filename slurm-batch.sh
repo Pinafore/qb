a8d66b08337abe6d822aef442ff99c3dfad2b937
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 16:43:28 -0700

    Try again

--- slurm-batch.sh
+++ slurm-batch.sh
@@ -2,11 +2,12 @@
 
 #SBATCH --job-name=qanta
 #SBATCH --qos=gpu
+#SBATCH --nodes=5
 #SBATCH --gres=gpu:5
 
-srun -c "slurm-run.sh 0" &
-srun -c "slurm-run.sh 1" &
-srun -c "slurm-run.sh 2" &
-srun -c "slurm-run.sh 3" &
-srun -c "slurm-run.sh 4" &
+srun -N 1 -c "slurm-run.sh 0" &
+srun -N 1 -c "slurm-run.sh 1" &
+srun -N 1 -c "slurm-run.sh 2" &
+srun -N 1 -c "slurm-run.sh 3" &
+srun -N 1 -c "slurm-run.sh 4" &
 wait
