3f99f4bdbc547c94c7a6162bb39787f66b249e8b
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 17:15:01 -0700

    Fix scripts

--- slurm-batch.sh
+++ slurm-batch.sh
@@ -9,10 +9,11 @@
 #SBATCH --mem 16gb
 #SBATCH --ntasks-per-node=4
 #SBATCH --error qanta.out.%j
+#SBATCH --ntasks=12
 
-srun -N 1 bash /fs/clip-quiz/qb/slurm-run.sh 0 &
-srun -N 1 bash /fs/clip-quiz/qb/slurm-run.sh 1 &
-srun -N 1 bash /fs/clip-quiz/qb/slurm-run.sh 2 &
-srun -N 1 bash /fs/clip-quiz/qb/slurm-run.sh 3 &
-srun -N 1 bash /fs/clip-quiz/qb/slurm-run.sh 4 &
+srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 0 &
+srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 1 &
+srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 2 &
+srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 3 &
+srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 4 &
 wait
