7d2589c22b67cc4bf77d64c7bb1a50f75058f549
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 17:20:47 -0700

    Fix misunderstanding in slurm

--- slurm-batch.sh
+++ slurm-batch.sh
@@ -2,18 +2,13 @@
 
 #SBATCH --job-name=qanta
 #SBATCH --qos=gpu
-#SBATCH --nodes=3
-#SBATCH --gres=gpu:2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --output qanta.out.%j
 #SBATCH --mem 16gb
 #SBATCH --ntasks-per-node=4
 #SBATCH --error qanta.out.%j
-#SBATCH --ntasks=12
+#SBATCH --ntasks=4
 
-srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 0 &
-srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 1 &
-srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 2 &
-srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 3 &
-srun -N 1 --gres=gpu:1 --qos=gpu --partition=gpu bash /fs/clip-quiz/qb/slurm-run.sh 4 &
-wait
+srun bash /fs/clip-quiz/qb/slurm-run.sh $1
---
+++ slurm-submit.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+bash slurm-batch.sh 0
+bash slurm-batch.sh 1
+bash slurm-batch.sh 2
+bash slurm-batch.sh 3
+bash slurm-batch.sh 4
