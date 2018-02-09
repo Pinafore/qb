958ecdc305c176d420081fd0aacd3d1f6ce8971f
EntilZha <ski.rodriguez@gmail.com>
2018-02-08 17:24:11 -0700

    Background scripts and wait at end

--- slurm-submit.sh
+++ slurm-submit.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-bash slurm-batch.sh 0
-bash slurm-batch.sh 1
-bash slurm-batch.sh 2
-bash slurm-batch.sh 3
-bash slurm-batch.sh 4
+bash slurm-batch.sh 0 &
+bash slurm-batch.sh 1 &
+bash slurm-batch.sh 2 &
+bash slurm-batch.sh 3 &
+bash slurm-batch.sh 4 &
+wait
