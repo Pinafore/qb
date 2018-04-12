577e3a147133bde07a8addcbb73210800e7243fb
sjtufs <sjtufs@gmail.com>
2018-04-11 23:07:17 -0400

    load protobowl questions also

--- qanta/buzzer/constants.py
+++ qanta/buzzer/constants.py
@@ -9,7 +9,8 @@ WORDVEC_DIR = 'data/external/GoogleNews-vectors-negative300.bin'
 WORDVEC_DIM = 300
 
 BUZZES_DIR = 'output/buzzer/{0}_buzzes_{1}.pkl'
-PROTOBOWL_DIR = 'data/external/qanta-4may17.log.json'
+# PROTOBOWL_DIR = 'data/external/qanta-4may17.log.json'
+PROTOBOWL_DIR = 'data/external/qanta-buzz.log'
 
 GUESSER_ACC_POS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 GUESSER_ACC = [0.0, 0.4469487280875181, 0.7067352049558455, 0.8356399103730064,
