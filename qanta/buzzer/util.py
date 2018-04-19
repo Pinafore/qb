aa913fbeb5bd531981b6a01d2ea727c87db26e24
sjtufs <sjtufs@gmail.com>
2018-04-19 02:17:48 -0400

    wip

--- qanta/buzzer/util.py
+++ qanta/buzzer/util.py
@@ -1,6 +1,9 @@
-import numpy
-import chainer
+import os
+import pickle
+import numpy as np
 import pandas as pd
+import chainer
+from tqdm import tqdm
 from chainer.backends import cuda
 from qanta.datasets.quiz_bowl import QuestionDatabase
 from qanta.guesser.abstract import AbstractGuesser
@@ -15,9 +18,11 @@ def read_data(
         guesser_module, guesser_class, guesser_config_num, '')
     questions = QuestionDatabase().all_questions()
     datasets = []
-    for fold in [BUZZER_TRAIN_FOLD, BUZZER_DEV_FOLD]:
+    # for fold in [BUZZER_TRAIN_FOLD, BUZZER_DEV_FOLD]:
+    for fold in [BUZZER_DEV_FOLD, BUZZER_DEV_FOLD]:
         datasets.append([])
         output_path = AbstractGuesser.guess_path(guesser_directory, fold)
+        '''
         df = pd.read_pickle(output_path)
         # group by qnum, sort by word position, then list of vectors and labels
         df_grouped = df.groupby('qnum')
@@ -29,7 +34,21 @@ def read_data(
             for _, row in q_group.loc[index.index].iterrows():
                 vectors.append(row.probs)
                 labels.append(row.guess == answer)
-            datasets[-1].append((vectors, labels))
+            datasets[-1].append((qid, vectors, labels))
+        '''
+        with open(output_path, 'rb') as f:
+            df = pickle.load(f)
+        for qid, q_group in tqdm(df.items()):
+            vectors = []
+            labels = []
+            answer = questions[qid].page
+            q_group = sorted(q_group.items(), key=lambda x: x[0])
+            for _, (probs, guess) in q_group:
+                vectors.append(probs)
+                labels.append(guess == answer)
+            vectors = np.asarray(vectors, dtype=np.float32)
+            labels = np.asarray(labels, dtype=np.int32)
+            datasets[-1].append((qid, vectors, labels))
     return datasets
 
 
@@ -46,11 +65,13 @@ def convert_seq(batch, device=None):
         else:
             xp = cuda.cupy.get_array_module(*batch)
             concat = xp.concatenate(batch, axis=0)
-            sections = numpy.cumsum([len(x)
-                                     for x in batch[:-1]], dtype=numpy.int32)
+            sections = np.cumsum([len(x) for x in batch[:-1]], dtype=np.int32)
             concat_dev = chainer.dataset.to_device(device, concat)
             batch_dev = cuda.cupy.split(concat_dev, sections)
             return batch_dev
 
-    return {'xs': to_device_batch([x for x, _ in batch]),
-            'ys': to_device_batch([y for _, y in batch])}
+    return {'xs': to_device_batch([x for _, x, _ in batch]),
+            'ys': to_device_batch([y for _, _, y in batch])}
+
+if __name__ == '__main__':
+    train, valid = read_data()
