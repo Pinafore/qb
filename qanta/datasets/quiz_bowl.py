b1ea17baaa44b0b6554947fe62766b5f6e498557
Pedro Rodriguez <ski.rodriguez@gmail.com>
2018-04-21 15:26:55 -0400

    Initial work on new qanta database class

--- qanta/datasets/quiz_bowl.py
+++ qanta/datasets/quiz_bowl.py
@@ -1,13 +1,15 @@
 from typing import List, Dict, Iterable, Optional, Any, Set, NamedTuple
 import json
-import os
-import sqlite3
 from collections import defaultdict, Counter
 
 from qanta import qlogging
 from qanta.datasets.abstract import AbstractDataset, TrainingData
-from qanta.util.environment import QB_QUESTION_DB
-from qanta.util import constants as c
+from qanta.util.constants import (
+    QANTA_MAPPED_DATASET_PATH,
+    GUESSER_TRAIN_FOLD, GUESSER_DEV_FOLD, BUZZER_TRAIN_FOLD, BUZZER_DEV_FOLD,
+    SYSTEM_DEV_FOLD, SYSTEM_TEST_FOLD,
+    TRAIN_FOLDS, DEV_FOLDS
+)
 from qanta.config import conf
 
 
@@ -47,132 +49,25 @@ class Question(NamedTuple):
 
 
 class QantaDatabase:
-    pass
+    def __init__(self, dataset_path=QANTA_MAPPED_DATASET_PATH):
+        with open(dataset_path) as f:
+            self.dataset = json.load(f)
 
+        self.version = self.dataset['version']
+        self.raw_questions = self.dataset['questions']
+        self.all_questions = [Question(**q) for q in self.raw_questions]
+        self.mapped_questions = [q for q in self.all_questions if q.page is not None]
 
-class QuestionDatabase:
-    def __init__(self):
-        pass
+        self.train_questions = [q for q in self.mapped_questions if q.fold in TRAIN_FOLDS]
+        self.guess_train_questions = [q for q in self.train_questions if q.fold == GUESSER_TRAIN_FOLD]
+        self.buzz_train_questions = [q for q in self.train_questions if q.fold == BUZZER_TRAIN_FOLD]
 
-    def query(self, command: str, arguments) -> Dict[str, Question]:
-        questions = {}
-        c = self._conn.cursor()
-        command = 'select id, page, category, answer, ' + \
-            'tournament, naqt, protobowl, fold ' + command
-        c.execute(command, arguments)
+        self.dev_questions = [q for q in self.mapped_questions if q.fold in DEV_FOLDS]
+        self.guess_dev_questions = [q for q in self.dev_questions if q.fold == GUESSER_DEV_FOLD]
+        self.buzz_dev_questions = [q for q in self.dev_questions if q.fold == BUZZER_DEV_FOLD]
+        self.system_dev_questions = [q for q in self.dev_questions if q.fold == SYSTEM_DEV_FOLD]
 
-        for qnum, page, category, answer, tournaments, naqt, protobowl, fold in c:
-            questions[qnum] = Question(qnum, answer, category, naqt, protobowl, tournaments, page, fold)
-
-        for q in self.expo_questions:
-            questions[q.qnum] = q
-
-        for qnum in questions:
-            command = 'select sent, raw from text where question=? order by sent asc'
-            c.execute(command, (qnum, ))
-            for sentence, text in c:
-                questions[qnum].add_text(sentence, text)
-
-        return questions
-
-    def all_questions(self, unfiltered=False):
-        if unfiltered:
-            return self.query('FROM questions', ())
-        else:
-            return self.query('FROM questions where page != ""', ())
-
-    def answer_map(self):
-        c = self._conn.cursor()
-        command = 'select answer, page from questions ' + \
-            'where page != ""'
-        c.execute(command)
-
-        d = defaultdict(Counter)
-        for answer, page in c:
-            d[answer][page] += 1
-
-        return d
-
-    @staticmethod
-    def normalize_answer(answer):
-        answer = answer.lower().replace("_ ", " ").replace(" _", " ").replace("_", "")
-        answer = answer.replace("{", "").replace("}", "")
-        answer = kPAREN.sub('', answer)
-        answer = kBRACKET.sub('', answer)
-        answer = kANGLE.sub('', answer)
-        answer = kMULT_SPACE.sub(' ', answer)
-        answer = " ".join(Question.split_and_remove_punc(answer))
-        return answer
-
-    def normalized_answers(self):
-        """
-        Return a dictionary with the most unmatched pages
-        """
-
-        c = self._conn.cursor()
-        command = 'select answer, page from questions '
-        c.execute(command)
-
-        answers = defaultdict(list)
-        for aa, page in c:
-            normalized = self.normalize_answer(aa)
-            answers[normalized].append((aa, page))
-        return answers
-
-    def questions_by_answer(self, answer):
-        questions = self.query('from questions where answer == ?', (answer,))
-
-        for ii in questions:
-            yield questions[ii]
-
-    def questions_with_pages(self) -> Dict[str, List[Question]]:
-        page_map = defaultdict(list) # type: Dict[str, List[Question]]
-        questions = self.query('from questions where page != ""', ()).values()
-
-        for q in questions:
-            page = q.page
-            page_map[page].append(q)
-        return page_map
-
-    def prune_text(self):
-        """
-        Remove sentences that do not have an entry in the database
-        """
-
-        c = self._conn.cursor()
-        command = 'select id from questions group by id'
-        c.execute(command)
-        questions = set(x for x in c)
-
-        c = self._conn.cursor()
-        command = 'select question from text group by question'
-        c.execute(command)
-        text = set(x for x in c)
-
-        orphans = text - questions
-
-        c = self._conn.cursor()
-        for ii in orphans:
-            command = 'delete from text where question=%i' % ii
-            c.execute(command)
-        log.info("Keeping %i Pruning %i" % (len(questions - orphans), len(orphans)))
-        self._conn.commit()
-
-    def all_answers(self):
-        """
-        Return a lookup from IDs to pages
-        """
-        answers = {}
-        c = self._conn.cursor()
-        command = "select id, page from questions where page != ''"
-        c.execute(command)
-
-        for qid, page in c:
-            answers[int(qid)] = page
-
-        for q in self.expo_questions:
-            answers[q.qnum] = q.page
-        return answers
+        self.test_questions = [q for q in self.mapped_questions if q.fold == SYSTEM_TEST_FOLD]
 
 
 class QuizBowlDataset(AbstractDataset):
