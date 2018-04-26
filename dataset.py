3f85e558a278af233e26ed4027baa875a6e788af
Pedro Rodriguez <ski.rodriguez@gmail.com>
2018-04-25 22:27:51 -0400

    Implement dataset upload, rename dataset management script

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ from os import path, makedirs
 
 DS_VERSION = '2018.04.18'
 S3_HTTP_PREFIX = 'https://s3-us-west-2.amazonaws.com/pinafore-us-west-2/qanta-jmlr-datasets/'
+S3_AWS_PREFIX = 's3://pinafore-us-west-2/qanta-jmlr/datasets/'
 LOCAL_PREFIX = 'data/external/datasets/'
 
 QANTA_UNMAPPED_DATASET_PATH = f'qanta.unmapped.{DS_VERSION}.json'
@@ -17,25 +18,42 @@ QANTA_DEV_DATASET_PATH = f'qanta.dev.{DS_VERSION}.json'
 QANTA_TEST_DATASET_PATH = f'qanta.test.{DS_VERSION}.json'
 
 FILES = [
-    (path.join(S3_HTTP_PREFIX, QANTA_UNMAPPED_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_UNMAPPED_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_PROCESSED_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_PROCESSED_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_MAPPED_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_MAPPED_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_SQLITE_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_SQLITE_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_TRAIN_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_TRAIN_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_DEV_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_DEV_DATASET_PATH)),
-    (path.join(S3_HTTP_PREFIX, QANTA_TEST_DATASET_PATH), path.join(LOCAL_PREFIX, QANTA_TEST_DATASET_PATH))
+    QANTA_UNMAPPED_DATASET_PATH,
+    QANTA_PROCESSED_DATASET_PATH,
+    QANTA_MAPPED_DATASET_PATH,
+    QANTA_SQLITE_DATASET_PATH,
+    QANTA_TRAIN_DATASET_PATH,
+    QANTA_DEV_DATASET_PATH,
+    QANTA_TEST_DATASET_PATH
 ]
+
+
+def make_file_pairs(source_prefix, target_prefix):
+    return [(path.join(source_prefix, f), path.join(target_prefix, f)) for f in FILES]
 
 
 def shell(command):
     return subprocess.run(command, check=True, shell=True, stderr=subprocess.STDOUT)
 
 
-@click.command()
+@click.group()
 def main():
+    pass
+
+
+@main.command()
+def download():
     makedirs(LOCAL_PREFIX, exist_ok=True)
-    for s3_file, local_file in FILES:
+    for s3_file, local_file in make_file_pairs(S3_HTTP_PREFIX, LOCAL_PREFIX):
+        print(f'Downloading {s3_file} to {local_file}')
         shell(f'wget -O {local_file} {s3_file}')
+
+
+@main.command()
+def upload():
+    for local_file, s3_file in make_file_pairs(LOCAL_PREFIX, S3_AWS_PREFIX):
+        print(f'Uploading {local_file} to {s3_file}')
+        shell(f'aws s3 cp {local_file} {s3_file}')
 
 
 if __name__ == '__main__':
