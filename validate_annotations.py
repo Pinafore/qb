78cdd726039a1842689ab8e00bb8bd2aa7d78e5b
Pedro Rodriguez <ski.rodriguez@gmail.com>
2018-05-09 23:39:42 -0400

    Improve validation script, add in safe disambiguation method

--- validate_annotations.py
+++ validate_annotations.py
@@ -1,38 +1,40 @@
+from qanta import qlogging
 from qanta.ingestion.answer_mapping import read_wiki_titles
 from qanta.ingestion.annotated_mapping import PageAssigner
+
+
+log = qlogging.get('validate_annotations')
 
 
 def normalize(title):
     return title.replace(' ', '_')
 
 
+def check_page(page, titles):
+    n_page = normalize(page)
+    if n_page not in titles:
+        log.error(f'Title not found: {page}')
+
+
 def main():
     titles = read_wiki_titles()
     assigner = PageAssigner()
-    print('Checking direct protobowl mappings...')
+    log.info('Checking direct protobowl mappings...')
     for page in assigner.protobowl_direct.values():
-        page = normalize(page)
-        if page not in titles:
-            print(f'Title not found: {page}')
+        check_page(page, titles)
 
-    print('Checking direct quizdb mappings...')
+    log.info('Checking direct quizdb mappings...')
     for page in assigner.quizdb_direct.values():
-        page = normalize(page)
-        if page not in titles:
-            print(f'Title not found: {page}')
+        check_page(page, titles)
 
-    print('Checking unambiguous mappings...')
+    log.info('Checking unambiguous mappings...')
     for page in assigner.unambiguous.values():
-        page = normalize(page)
-        if page not in titles:
-            print(f'Title not found: {page}')
+        check_page(page, titles)
 
-    print('Checking ambiguous mappings...')
+    log.info('Checking ambiguous mappings...')
     for entry in assigner.ambiguous.values():
         for option in entry:
-            page = normalize(option['page'])
-            if page not in titles:
-                print(f'Title not found: {page}')
+            check_page(option['page'], titles)
 
 
 if __name__ == '__main__':
