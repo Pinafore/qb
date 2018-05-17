8113cd342dd2b1521777af15b5d169bb9b042bd9
EntilZha <ski.rodriguez@gmail.com>
2018-05-17 14:23:50 -0600

    Fix script

--- bin/trickme.sh
+++ bin/trickme.sh
@@ -2,7 +2,7 @@
 
 set -e
 sqlite3 -header -csv data/external/non_naqt.db < bin/non_naqt_to_csv.sql > data/external/non_naqt.csv
-./cli.py data/external/non_naqt.csv data/external/datasets/
+./cli.py nonnaqt_to_json data/external/non_naqt.csv data/external/datasets/
 mkdir -p .data/quizbowl
 cp data/external/datasets/qanta.torchtext* .data/quizbowl
-./cli.py data/external/first_round_adversarial.txt data/external/datasets
+./cli.py adversarial_to_json data/external/first_round_adversarial.txt data/external/datasets
