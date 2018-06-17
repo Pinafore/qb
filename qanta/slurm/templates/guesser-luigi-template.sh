5360d40058502acc0769997230226453f5ae995e
EntilZha <ski.rodriguez@gmail.com>
2018-06-17 15:10:53 -0700

    Better log location

--- qanta/slurm/templates/guesser-luigi-template.sh
+++ qanta/slurm/templates/guesser-luigi-template.sh
@@ -5,8 +5,8 @@
 #SBATCH --qos={{ qos }}
 #SBATCH --mem-per-cpu={{ mem_per_cpu }}
 #SBATCH --chdir=/fs/clip-quiz/entilzha/qb
-#SBATCH --output=/fs/clip-quiz/entilzha/slurm-out/slurm-%A_%a.out
-#SBATCH --error=/fs/clip-quiz/entilzha/slurm-err/slurm-%A_%a.out
+#SBATCH --output=/fs/clip-quiz/entilzha/slurm-logs/stdout-%A_%a.out
+#SBATCH --error=/fs/clip-quiz/entilzha/slurm-logs/stderr-%A_%a.out
 #SBATCH --time={{ max_time }}
 {% if gres %}
 #SBATCH --gres={{ gres }}
