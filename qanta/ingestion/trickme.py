7b0b9114eb7b27a0739b645217a682851d4e5072
EntilZha <ski.rodriguez@gmail.com>
2018-10-26 17:44:46 -0400

    Fix mappings

--- qanta/ingestion/trickme.py
+++ qanta/ingestion/trickme.py
@@ -3,21 +3,24 @@ from qanta.ingestion.preprocess import format_qanta_json, add_sentences_
 
 DIRECT_MAP = {
     2: ('syracuse', 'Syracuse,_Sicily'),
+    40: ('fyodor_dostoyevsky', 'Fyodor_Dostoevsky'),
     80: ('james_maxwell', 'James_Clerk_Maxwell'),
     106: ('syracuse', 'Syracuse,_Sicily'),
     131: ('the_dead', 'The_Dead_(short_story)'),
     144: ('if', None),
-    169: ('the_stranger', None),
-    171: ('the_stranger', None),
-    172: ('the_stranger', None),
-    173: ('the_stranger', None),
-    188: ('uber_(company)', 'Uber_(company)'),
+    169: ('the_stranger', 'The_Stranger_(novel)'),
+    171: ('the_stranger', 'The_Stranger_(novel)'),
+    172: ('the_stranger', 'The_Stranger_(novel)'),
+    173: ('the_stranger', 'The_Stranger_(novel)'),
+    188: ('uber_(company)', 'Uber'),
     205: ('gone_with_the_wind', None),
     207: ('liberal_democratic_party', None),
     215: ('readymades', None),
     272: ('gel_permeation_chromatography', None),
+    279: ('nuclear_membrane', 'Nuclear_envelope'),
     291: ('dienes', None),
     294: ('nitro', 'Nitro_compound'),
+    310: ('folic_acid', 'Folate'),
     314: ('polarity', 'Chemical_polarity'),
     317: ('ring', None),
     360: ('the_president', 'President_of_the_United_States'),
@@ -45,10 +48,13 @@ DIRECT_MAP = {
     669: ('bowling_ball', 'Bowling_Balls'),
     690: ('hemlock', 'Conium_maculatum'),
     692: ('tcp', 'Transmission_Control_Protocol'),
+    695: ('angels_in_america:_a_gay_fantasia_on_national_themes', 'Angels_in_America'),
+    696: ('angels_in_america:_a_gay_fantasia_on_national_themes', 'Angels_in_America'),
     693: ('hemlock', 'Conium_maculatum'),
     734: ('jackson', None),
     757: ('jackson', None),
     767: ('centaurs', 'Centaur'),
+    768: ('angels_in_america:_a_gay_fantasia_on_national_themes', 'Angels_in_America'),
     772: ('bowling_ball', 'Bowling_Balls'),
     774: ('bowling_ball', 'Bowling_Balls')
 }
@@ -68,7 +74,7 @@ class Trickme:
             for i, q in enumerate(json.load(f)):
                 text = q['Question']
                 answer = q['Answer'].replace(' ', '_')
-                if len(answer) == 0:
+                if len(answer) == 0 or len(text) == 0:
                     continue
                 if i in DIRECT_MAP:
                     m_ans, m_page = DIRECT_MAP[i]
