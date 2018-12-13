7c1892a4fd3d793fb2a9abf64c6dcd8e5b751063
Jordan Boyd-Graber <jordanbg@gmail.com>
2018-12-13 15:36:01 -0500

    waiting for Chen's new files

--- qanta/expo/buzzer.py
+++ qanta/expo/buzzer.py
@@ -2,8 +2,10 @@ from __future__ import print_function
 import textwrap
 from collections import defaultdict
 import argparse
+import random
 from csv import DictReader
 from time import sleep
+import datetime
 #from str import lower
 from random import shuffle
 import sys
@@ -226,7 +228,7 @@ class kCOLORS:
         start = getattr(kCOLORS, color)
         print(start + text + kCOLORS.ENDC, end=end)
 
-def write_readable(filename, ids, questions, power):
+def write_readable(filename, ids, questions):
     question_num = 0
     o = open(flags.readable, 'w')
     for ii in question_ids:
@@ -234,7 +236,7 @@ def write_readable(filename, ids, questions, power):
         o.write("%i) " % question_num)
         power_found = False
         for jj in questions[ii]:
-            if power(ii) and not power_found and power(ii).lower() in questions[ii][jj].lower():
+            if questions._power(ii) and not power_found and questions._power(ii).lower() in questions[ii][jj].lower():
                 power_found = True
                 o.write("%s  " %
                         questions[ii][jj].replace(power(ii), "(*) %s" %
@@ -251,15 +253,19 @@ def clear_screen():
 class PowerPositions:
     def __init__(self, filename):
         self._power_marks = {}
-        try:
-            infile = DictReader(open(filename, 'r'))
-            for ii in infile:
-                question = int(ii['question'])
-                self._power_marks[question] = ii['word']
-        except:
-            print("Couldn't load from %s" % filename)
-        print("Read power marks from %s: %s ..." %
-              (filename, str(self._power_marks.keys())[1:69]))
+        if filename:
+            try:
+                infile = DictReader(open(filename, 'r'))
+                for ii in infile:
+                    question = int(ii['question'])
+                    self._power_marks[question] = ii['word']
+            except:
+                print("Couldn't load from %s" % filename)
+            print("Read power marks from %s: %s ..." %
+                (filename, str(self._power_marks.keys())[1:69]))
+        else:
+            print("No power marks")
+            
     def __call__(self, question):
         if question in self._power_marks:
             return self._power_marks[question]
@@ -336,24 +342,73 @@ def show_score(left_score, right_score,
 
 
 class Guess:
-    def __init__(self, page, evidence, final, weight):
+    def __init__(self, system, page, evidence, final, weight):
+        self.system = system
         self.page = page.replace("_", " ")
         self.evidence = evidence
         self.final = final
         self.weight = weight
 
 class Buzzes:
-    def __init__(self, buzz_file):
-        buzzfile = DictReader(open(buzz_file, 'r'))
-
+    def __init__(self, file_path):
         self._buzzes = defaultdict(dict)
+        self._finals = defaultdict(dict)
+        print("Initializing buzz files")
+
+    def debug(self):
+        self.add_guess(0, 0, 5, "A", "Heisenberg", "", 0, .2)
+        self.add_guess(0, 0, 5, "C", "Narcos", "", 0, .2)
+        self.add_guess(0, 2, 3, "A", "Better_Call_Saul", "", 1, .7)
+        self.add_guess(0, 2, 3, "B", "Breaking Bad", "", 1, .6)
+        self.add_guess(0, 2, 3, "C", "Breaking Bad", "", 1, .5)
+        self._finals[0]["A"] = "Breaking Bad"
+        self._finals[0]["B"] = "Breaking Bad"
+        self._finals[0]["C"] = "Breaking Bad"        
+
+        self.add_guess(1, 0, 5, "A", "SimCity", "", 0, .2)
+        self.add_guess(1, 0, 5, "C", "Skyrim", "", 0, .2)
+        self.add_guess(1, 2, 3, "A", "Skyrim", "", 0, .7)
+        self.add_guess(1, 3, 3, "B", "Jedi Knight", "", 0, .6)
+        self.add_guess(1, 3, 3, "C", "Jedi Knight", "", 0, .5)
+        self._finals[1]["A"] = "Fallout 76"
+        self._finals[1]["B"] = "Fallout 76"
+        self._finals[1]["C"] = "Fallout (series)"        
+
+        self.add_guess(2, 0, 5, "A", "Apple", "", 0, .2)
+        self.add_guess(2, 0, 5, "C", "Onion", "", 0, .2)
+        self.add_guess(2, 0, 5, "C", "Apple", "", 0, .2)
+        self.add_guess(2, 2, 3, "A", "Apple", "", 1, .7)
+        self.add_guess(2, 3, 3, "B", "Onion", "", 0, .6)
+        self.add_guess(2, 3, 3, "C", "Jedi Knight", "", 0, .5)
+        self._finals[2]["A"] = "Potato"
+        self._finals[2]["B"] = "Potato"
+        self._finals[2]["C"] = "Potato"  
+
+    def add_guess(self, question, sent, word, system, guess, evidence, final, weight):
+        if not (sent, word) in self._buzzes[question]:
+            self._buzzes[question][(sent, word)] = {}
+        self._buzzes[question][(sent, word)][guess] = \
+            Guess(system,guess, evidence, final, weight)
+        
+    def add_system(self, file_path):
+        self.load_finals("%s.finals" % file_path)
+        
+        buzzfile = DictReader(open("%s.buzz" % buzz_file, 'r'))
+        system = buzzfile.split('/')[-1]
+        system = buzzfile.split('.')[0]
+        system = buzzfile.split("_")[0]
+
         for ii in buzzfile:
             question, sent, word = int(ii["question"]), int(ii["sentence"]), int(ii["word"])
-            if not (sent, word) in self._buzzes[question]:
-                self._buzzes[question][(sent, word)] = {}
-            self._buzzes[question][(sent, word)][ii["page"]] = \
-              Guess(ii["page"], ii["evidence"], int(ii["final"]), float(ii["weight"]))
+            self.add_guess(question, sent, word, system, ii["page"], ii["evidence"],
+                           int(ii["final"]), float(ii["weight"]))
 
+    def load_finals(self, system, final_file):
+        ff = DictReader(open(final_file))
+        for ii in ff:
+            self._finals[int(ii['question'])][system] = ii['answer'].replace('_', ' ')
+
+            
     def current_guesses(self, question, sent, word):
         try:
             ss, ww = max(x for x in self._buzzes[question] if
@@ -368,20 +423,38 @@ class Buzzes:
         for ii in self._buzzes:
             yield ii
 
-    def final_guess(self, question):
-        for ss, ww in sorted(self._buzzes[question], reverse=True):
-            for bb in self._buzzes[question][(ss, ww)]:
-                if self._buzzes[question][(ss, ww)][bb].final:
-                    return bb
-        return None
-
-
 class Questions:
-    def __init__(self, question_file):
-        qfile = DictReader(open(question_file, 'r'))
-
+    def __init__(self):
         self._questions = defaultdict(dict)
         self._answers = defaultdict(str)
+        self._power = PowerPositions("")
+        print("Initializing questions")
+
+    def debug(self):
+        self._questions[0] = {0: "His aliases include: Pastor Hansford of Free Will Baptist in Coushatta, Louisianna; Viktor with a K St. Claire, a South African who just inherited money from his uncle; Charlie Hustle, a mailboy in the HHM mailroom; and Gene Takavic, a Cinnabon manager in Omaha.",
+                              1: "His best known alias came from selling burner phones in Albuquerque, New Mexico and was more fitting for a lawyer than his birthname, James McGill.",
+                              2: "For ten points, name this titular CRIMINAL lawyer from an AMC series who originated on Breaking Bad."}
+        self._answers[0] = "Better Call Saul"
+
+        self._questions[1] = {0: 'This game contains an easter egg based on fan art of blocks spelling out "P A M" and an insect in a top hat, and its launch was declared a holiday by Jim Justice on November 14, 2018.',  
+                              1: 'The "Personal Matters" quest requires the player to kill Evan, who was the fiancé of the Overseer.  Locations in this game include the Whitespring Resort, which is based on the real-life Greenbrier resort in Solphur Springs, West Virginia.',
+                              2: "A multiplayer game centered on a control vault, for ten points name Bathesda's most recent entry in the Fallout franchise."}
+        self._answers[1] = "Fallout 76"
+
+        self._questions[2] = {0: "Amsterdam experienced a riot named for this crop during World War One.",
+                              1: "In the 1830s and 40s, Russian peasants on the lower Volga objected to the forced introduction of this crop in a namesake series of riots.",
+                              2: "Antoine-Augustin Parmentier pushed for the cultivation of this crop in France.",
+                              3: "This crop provides an alternate name for the War of the Bavarian Succession.",
+                              4: "An epidemic of phytophthora infestans devastated the cultivation of this crop in the mid-19th Century.",
+                              5: "Robert Peel repealed the Corn Laws to counteract a blight of, for 10 points, what crop that caused an Irish famine?"}
+        self._answers[2] = "Potato"
+
+    def load_power(self, power_file):
+        self._power = PowerPositions(power_file)
+    
+    def load_questions(self, question_file):
+        qfile = DictReader(open(question_file, 'r'))
+
         for ii in qfile:
             self._questions[int(ii["id"])][int(ii["sent"])] = ii["text"]
             self._answers[int(ii["id"])] = ii["answer"].strip().replace("_", " ")
@@ -413,17 +486,11 @@ def format_display(display_num, question_text, sent, word, current_guesses,
     for gg in sorted(current_guesses, key=lambda x: current_guesses[x].weight, reverse=True)[:guess_limit]:
         guess = current_guesses[gg]
         if guess.page == answer:
-            report += "%s\t%f\t%s\n" % ("***CORRECT***", guess.weight, guess.evidence[:60])
+            report += "%-10s\t%-50s\t%0.2f\t%s\n" % (guess.system, "***CORRECT***", guess.weight, guess.evidence[:60])
         else:
-            report += "%s\t%f\t%s\n" % (guess.page, guess.weight, guess.evidence[:60])
+            report += "%-10s\t%-50s\t%0.2f\t%s\n" % (guess.system, guess.page, guess.weight, guess.evidence[:60])
     return report
 
-def load_finals(final_file):
-    ff = DictReader(open(final_file))
-    d = {}
-    for ii in ff:
-        d[int(ii['question'])] = ii['answer'].replace('_', ' ')
-    return d
 
 
 def interpret_keypress(other_allowable=""):
@@ -449,9 +516,9 @@ def interpret_keypress(other_allowable=""):
     return press
 
 
-def answer(ans, print_string="OUSIA says:"):
-    if print_string:
-        print(print_string)
+def answer(ans, system):
+    if system:
+        print("%s says:" % system)
     os.system("afplay /System/Library/Sounds/Glass.aiff")
     os.system("say -v Tom %s" % ans.replace("'", "").split("(")[0])
     sleep(kPAUSE)
@@ -467,12 +534,28 @@ def present_question(display_num, question_id, question_text, buzzes, final,
     for ss in sorted(question_text):
         words = question_text[ss].split()
         for ii, ww in enumerate(words):
+            # if we've reached the end of the question
+            if ss == max(question_text) and ii == len(question_text[ss].split()) - 1:
+                 # If humans haven't buzzed, let the computer buzz
+                 if computer_delta == 0:
+                    print(final)
+                    system = random.choice(list(final.keys()))
+                    answer(final[system].split('(')[0], system)
+                    final = final[system]
+                    if final == correct:
+                        return (human + human_delta, computer + 10, final)
+                    else:
+                        print("Incorrect answer: %s" % final)
+                 else:
+                     words += [" ", " ", " ", " ", " "] 
+            
             if ww.lower().startswith(power.lower()):
                 question_value = 10
             press = interpret_keypress()
             current_guesses = buzzes.current_guesses(question_id, ss, ii - 2)
             buzz_now = [x for x in current_guesses.values() if x.final]
-            assert len(buzz_now) < 2, "Cannot buzz on more than one thing"
+            # Removing this assertion now that we can have multiple systems playing
+            # assert len(buzz_now) < 2, "Cannot buzz on more than one thing"
             if isinstance(press, int):
                 os.system("afplay /System/Library/Sounds/Glass.aiff")
                 response = None
@@ -499,7 +582,7 @@ def present_question(display_num, question_id, question_text, buzzes, final,
                 print(format_display(display_num, question_text, ss, ii + 1,
                                      current_guesses, answer=correct,
                                      points=question_value))
-                answer(buzz_now[0].page.split('(')[0])
+                answer(buzz_now[0].page.split('(')[0], buzz_now[0].system)
                 if buzz_now[0].page == correct:
                     print("Computer guesses: %s (correct)" % buzz_now[0].page)
                     sleep(1)
@@ -523,12 +606,6 @@ def present_question(display_num, question_id, question_text, buzzes, final,
                 print(format_display(display_num, question_text, ss, ii + 1,
                                      current_guesses, answer=correct,
                                      points=question_value))
-    if computer_delta == 0:
-        answer(final.split('(')[0])
-        if final == correct:
-            return (human + human_delta, computer + 10, final)
-        else:
-            print("Incorrect answer: %s" % final)
 
     if human_delta == 0:
         response = None
@@ -547,35 +624,49 @@ def present_question(display_num, question_id, question_text, buzzes, final,
 
     return (human + human_delta, computer + computer_delta, "")
 
-
-
-if __name__ == "__main__":
+def create_parser():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--questions', type=str, default='qanta/expo/question.csv')
-    parser.add_argument('--buzzes', type=str, default="qanta/expo/buzz.csv")
-    parser.add_argument('--output', type=str, default="qanta/expo/competition.csv")
-    parser.add_argument('--finals', type=str, default="qanta/expo/final.csv")
+    parser.add_argument('--questions', type=str, default='')
+    parser.add_argument('--model_directory', type=str, default="")
+    parser.add_argument('--model_name', type=str, default="")    
+    parser.add_argument('--output', type=str, default="GAMEPLAY %s.csv" % datetime.datetime.now().strftime("%I:%M%p on %B %d %Y"))
     parser.add_argument('--players', type=int, default=1)
     parser.add_argument('--human_start', type=int, default=0)
     parser.add_argument('--computer_start', type=int, default=0)
     parser.add_argument('--skip', type=int, default=0)
-    parser.add_argument('--power', type=str, default="power.csv")
+    parser.add_argument('--power', type=str, default="")
     parser.add_argument('--max_questions', type=int, default=40)
     parser.add_argument('--readable', type=str, default="readable.txt")
+    return parser.parse_args()
 
-    flags = parser.parse_args()
+def load_data(flags):
+    questions = Questions()
+    buzzes = Buzzes(flags.model_directory)
+    
+    if flags.questions != "":
+        questions.load_questions(flags.questions)
+        questions.load_power(flags.power)
+    else:
+        questions.debug()
 
-    questions = Questions(flags.questions)
-    buzzes = Buzzes(flags.buzzes)
-    finals = load_finals(flags.finals)
-    power = PowerPositions(flags.power)
-    print("Done loading data")
-    clear_screen()
+    if flags.model_directory != "":
+        for ii in glob("%s/*" % flags.model_drectory):
+            if ii.endswith(".buzz.csv"):
+                # if we have a specific model, only load that
+                if flags.model != "" and "%s.buzz.csv" % flags.model not in ii:
+                    print("Not loading model %s" % ii)
+                else:
+                    buzzes.add_system(ii.replace(".buzz.csv", ""))
+    else:
+        buzzes.debug()
 
+    return questions, buzzes
+
+def buzzer_check(players):
+    if players > 0:
+        print("Time for a buzzer check")
+    players_needed = range(1, players + 1)
     current_players = set()
-
-    print("Time for a buzzer check")
-    players_needed = range(1, flags.players + 1)
     while len(current_players) < len(players_needed):
         print("Player %i, please buzz in" % min(x for x in players_needed \
                                                 if x not in current_players))
@@ -585,9 +676,17 @@ if __name__ == "__main__":
             print("Thanks for buzzing in, player %i!" % press)
             current_players.add(press)
  
-    if flags.players > 0:
+    if players > 0:
         sleep(1.5)
-        answer("I'm ready too")
+        answer("I'm ready too", "QANTA")
+
+if __name__ == "__main__":
+    flags = create_parser()
+    questions, buzzes = load_data(flags)
+    print("Done loading data")
+    
+    clear_screen()
+    buzzer_check(flags.players)
 
     human = flags.human_start
     computer = flags.computer_start
@@ -597,23 +696,23 @@ if __name__ == "__main__":
     question_ids = [x for x in question_ids if x in buzzes]
 
     if flags.readable != "":
-        write_readable(flags.readable, question_ids, questions, power)
+        write_readable(flags.readable, question_ids, questions)
 
     for ii in question_ids:
         question_num += 1
         if flags.skip > 0 and question_num < flags.skip:
             continue
 
-        power_mark = power(ii)
+        power_mark = questions._power(ii)
         if power_mark == "10":
             print("Looking for power for %i, got %s %s" %
                   (ii, power_mark, str(ii in power._power_marks.keys())))
         hum, comp, ans = present_question(question_num, ii, questions[ii],
-                                          buzzes, finals[ii],
+                                          buzzes, buzzes._finals[ii],
                                           questions.answer(ii),
                                           human=human,
                                           computer=computer,
-                                          power=power(ii))
+                                          power=questions._power(ii))
         human = hum
         computer = comp
 
