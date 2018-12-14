0f0394dd5cba3be53fa43cae92040fecc1dbf76d
Jordan Boyd-Graber <jordanbg@gmail.com>
2018-12-13 19:32:23 -0500

    working on toy data

--- qanta/expo/2p_buzzer.py
+++ qanta/expo/2p_buzzer.py
@@ -4,51 +4,42 @@ from collections import defaultdict
 import argparse
 from csv import DictReader
 from time import sleep
-from string import lower
 from random import shuffle
 import sys
 import os
+import random
 
 from buzzer import kSHOW_RIGHT, kPAUSE, kBIGNUMBERS
 from buzzer import clear_screen, PowerPositions, show_score
 from buzzer import Guess, Buzzes, Questions, format_display
-from buzzer import load_finals, interpret_keypress, answer
+from buzzer import interpret_keypress, answer, create_parser
+from buzzer import load_data, buzzer_check, question_loop, Score
 
 
-class Score:
-    def __init__(self, even=0, odd=0, human=0, computer=0):
-        self.even = even
-        self.odd = odd
-        self.human = human
-        self.computer = computer
-
-    def add(self, score):
-        return Score(self.even + score.even,
-                     self.odd + score.odd,
-                     self.human + score.human,
-                     self.computer + score.computer)
-
-
-def present_question(display_num, question_id, question_text, buzzes, final,
-                     correct, score, power="10"):
+def present_question_hh(display_num, question_id, question_text, buzzes, final,
+                        correct, score, power="10"):
 
     even_delta = 0
     odd_delta = 0
     question_value = 15
+
+    final_system = random.choice(list(final.keys()))
+    final_answer = final[final_system]
 
     # Find out where the computer would buzz
     computer_position = (max(question_text) + 1, 0)
     for ss in sorted(question_text):
         words = question_text[ss].split()
         for ii, ww in enumerate(words):
-            current_guesses = buzzes.current_guesses(question_id, ss, ii)
+            current_guesses = buzzes.current_guesses(question_id, ss, ii - 1)
             buzz_now = [x for x in current_guesses.values() if x.final]
-            if len(buzz_now) == 1:
+            if len(buzz_now) > 0:
                 computer_position = ss, ii
                 computer_guess = buzz_now[0].page
+                computer_system = buzz_now[0].system
                 break
 
-    question_text[ss] += " WAIT WAIT WAIT WAIT WAIT "
+    question_text[ss] += "~ ~ ~ ~ ~"
 
     question_done = False
     human_delta = 0
@@ -63,25 +54,25 @@ def present_question(display_num, question_id, question_text, buzzes, final,
                 # This is where the computer buzzes
                 if human_delta <= 0:
                     if computer_guess == correct:
-                        os.system("afplay sounds/applause.wav")
+                        # os.system("afplay sounds/applause.wav")
                         if human_delta <= 0:
                             computer_delta = question_value
                     else:
-                        answer(computer_guess)
-                        os.system("afplay sounds/sad_trombone.wav")
+                        answer(computer_guess, computer_system)
+                        # os.system("afplay sounds/sad_trombone.wav")
                         if human_delta == 0:
                             computer_delta = -5
                 else:
-                    answer(computer_guess)
+                    answer(computer_guess, computer_system)
                     question_done = True
                     computer_delta = 0
 
             current_guesses = buzzes.current_guesses(question_id, ss, ii)
-            if lower(ww).startswith(lower(power)):
+            if ww.lower().startswith(power.lower()):
                 question_value = 10
             press = interpret_keypress()
 
-            if press:
+            if isinstance(press, int):
                 # Check to see if buzz is valid
                 if human_delta > 0:
                     continue
@@ -93,8 +84,8 @@ def present_question(display_num, question_id, question_text, buzzes, final,
                 os.system("afplay /System/Library/Sounds/Glass.aiff")
                 response = None
                 while response is None:
-                    response = raw_input("Player %i, provide an answer:\t"
-                                         % press)
+                    response = input("Player %i, provide an answer:\t"
+                                     % press)
                     if '+' in response:
                         if press % 2 == 0:
                             even_delta = question_value
@@ -139,75 +130,38 @@ def present_question(display_num, question_id, question_text, buzzes, final,
 
     # Now see what the computer would do
     if computer_delta == 0 and human_delta <= 0:
-        answer(final.split('(')[0])
+        answer(final_answer.split('(')[0], final_system)
         if final == correct:
             computer_delta = 10
         else:
             print("Computer guesses incorrectly: %s" % final)
     elif computer_delta > 0:
-        answer(computer_guess)
+        answer(computer_guess, computer_system)
         format_display(display_num, question_text, computer_position[0],
                        computer_position[1], current_guesses, answer=correct,
                        points=computer_delta)
 
-    return score.add(Score(even_delta, odd_delta, human_delta, computer_delta))
+    return(Score(even_delta, odd_delta, human_delta, computer_delta))
+
+def check_hh_tie(score):
+    """
+    For the computer-human and human-human programs, this needs to be
+    different.  This is why it's a silly function.
+    """
+    return score.even == score.odd
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--questions', type=str, default='questions.csv')
-    parser.add_argument('--buzzes', type=str, default="ir_buzz.csv")
-    parser.add_argument('--output', type=str, default="competition.csv")
-    parser.add_argument('--finals', type=str, default="finals.csv")
-    parser.add_argument('--power', type=str, default="power.csv")
-    parser.add_argument('--skip', type=int, default=0)
-    parser.add_argument('--max_questions', type=int, default=20)
-
-    flags = parser.parse_args()
-
-    questions = Questions(flags.questions)
-    buzzes = Buzzes(flags.buzzes)
-    finals = load_finals(flags.finals)
-    power = PowerPositions(flags.power)
+    flags = create_parser()
+    questions, buzzes = load_data(flags)
     print("Done loading data")
+    
     clear_screen()
+    buzzer_check(flags.players)
 
-    current_players = set()
+    score = question_loop(flags, questions, buzzes, present_question_hh, 
+                          check_hh_tie)
 
-    if False:
-        print("Time for a buzzer check")
-        players_needed = [1]
-        while len(current_players) < len(players_needed):
-            print("Player %i, please buzz in" % min(x for x in players_needed \
-                                                    if x not in current_players))
-            press = interpret_keypress()
-            if press in players_needed:
-                os.system("afplay /System/Library/Sounds/Glass.aiff")
-                print("Thanks for buzzing in, player %i!" % press)
-                current_players.add(press)
-
-        sleep(1.5)
-        answer("I'm ready too")
-
-    score = Score()
-    question_num = 0
-    question_ids = sorted(questions._questions.keys(), key=lambda x: x % 11)
-    question_ids = list(question_ids)[flags.skip:]
-    for ii in question_ids:
-        # print(ii, questions[ii])
-        question_num += 1
-        power_mark = power(ii)
-        score = present_question(question_num, ii, questions[ii],
-                                 buzzes, finals[ii],
-                                 questions.answer(ii),
-                                 score=score,
-                                 power=power(ii))
-
-        print("Correct answer of Question %i: %s" % (question_num,
-                                                     questions.answer(ii)))
-        sleep(kPAUSE)
-
-        if question_num > flags.max_questions - 1:
-            break
-
-    show_score(score.human, score.computer)
+    show_score(score.human, score.computer,
+               "HUMAN", "COMPUTER")
+            
